824e143b25a04e42fbf3471f3653ff6222f731e3
Vladislav Titov <povar359@gmail.com>
2019-02-22 16:38:09 +0300

    утстановка не пустых значений

--- js/script.js
+++ js/script.js
@@ -9,49 +9,49 @@ var checkin = popupForm.querySelector("[name=checkin]");
 var checkout = popupForm.querySelector("[name=checkout]");
 
 var isStorageSupport = true;
-  var adultsStorage = "";
-  var childrenStorage = "";
+var adultsStorage = "";
+var childrenStorage = "";
 
-  try {
+try {
     adultsStorage = localStorage.getItem("adults-count");
     childrenStorage = localStorage.getItem("children-count");
-  } catch (err) {
+} catch (err) {
     isStorageSupport = false;
-  }
+}
 
 
 popupButton.addEventListener("click", function (evt) {
     evt.preventDefault();
-    if (!popupOpen)
-    {
+    if (!popupOpen) {
         popupForm.classList.add("map-search-show");
-        if(popupForm.classList.contains("map-search-hide"))
+        if (popupForm.classList.contains("map-search-hide"))
             popupForm.classList.remove("map-search-hide");
         popupOpen = true;
     }
-    else
-    {
+    else {
         popupForm.classList.add("map-search-hide");
-        if(popupForm.classList.contains("map-search-show"))
+        if (popupForm.classList.contains("map-search-show"))
             popupForm.classList.remove("map-search-show");
         popupOpen = false;
     }
 
-    adults.value = adultsStorage;
-    children.value = childrenStorage;
+    if (adultsStorage)
+        adults.value = adultsStorage;
+    if (childrenStorage)
+        children.value = childrenStorage;
 
 });
 
 popupForm.addEventListener("submit", function (evt) {
     if (!checkin.value || !checkout.value) {
-      evt.preventDefault();
-      popupForm.classList.remove("modal-error");
-      popupForm.offsetWidth = popupForm.offsetWidth;
-      popupForm.classList.add("modal-error");
+        evt.preventDefault();
+        popupForm.classList.remove("modal-error");
+        popupForm.offsetWidth = popupForm.offsetWidth;
+        popupForm.classList.add("modal-error");
     } else {
-      if (isStorageSupport) {
-        localStorage.setItem("adults-count", adults.value);
-        localStorage.setItem("children-count", children.value);
-      }
+        if (isStorageSupport) {
+            localStorage.setItem("adults-count", adults.value);
+            localStorage.setItem("children-count", children.value);
+        }
     }
-  });
+});
